49046c55695472dfa71cfaa500f431cfa5a769e7
Kevin Hill <Kevin Hill>
2016-06-22 21:36:40 -0700

    Updating Service Worker detection logic

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,7 @@ document.addEventListener( "DOMContentLoaded", ready, false )
 
 function ready(e){
     //Check for Service Worker
-    if(typeof serviceWorker in navigator !== undefined){
+    if(typeof serviceWorker in navigator){
         console.log("Service Worker object is found");
     } else {
         console.log("Service Worker object is NOT found");
