7d62a3d3aa5637f524d265cfab8a381cfa749016
Kevin Hill <Kevin Hill>
2016-06-22 21:34:22 -0700

    Added basic service worker detection

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log('This would be the main JS file.');
+
+
+document.addEventListener( "DOMContentLoaded", ready, false )
+
+function ready(e){
+    //Check for Service Worker
+    if(typeof serviceWorker in navigator !== undefined){
+        console.log("Service Worker object is found");
+    } else {
+        console.log("Service Worker object is NOT found");
+    }
+}
